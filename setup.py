0536ba5066fe8fd5f9935fe18b0d44e1e8d9fe94
Jacob Griswold-Moran <jacob.griswold-moran@successacademies.org>
2017-08-21 11:39:49 -0400

    adds packages to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='envloader',
     version='0.1',
-    scripts=['envloader']
+    scripts=['envloader'],
+    packages=('EnvironLoader', 'EnvironLoader.EnvironLoader')
 )
